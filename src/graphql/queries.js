84f3fdffc3da2fa38e10a1ae328e8bbb9fa4b172
BhanuPrataSingh97 <bhanu.official97@gmail.com>
2022-12-19 00:28:19 +0530

    added all files

---
+++ src/graphql/queries.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      firstName
+      lastName
+      email
+      countryCode
+      mobileNumber
+      password
+      userType
+      loginType
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        firstName
+        lastName
+        email
+        countryCode
+        mobileNumber
+        password
+        userType
+        loginType
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,66 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser(
+    $filter: ModelSubscriptionUserFilterInput
+    $owner: String
+  ) {
+    onCreateUser(filter: $filter, owner: $owner) {
+      id
+      firstName
+      lastName
+      email
+      countryCode
+      mobileNumber
+      password
+      userType
+      loginType
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser(
+    $filter: ModelSubscriptionUserFilterInput
+    $owner: String
+  ) {
+    onUpdateUser(filter: $filter, owner: $owner) {
+      id
+      firstName
+      lastName
+      email
+      countryCode
+      mobileNumber
+      password
+      userType
+      loginType
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser(
+    $filter: ModelSubscriptionUserFilterInput
+    $owner: String
+  ) {
+    onDeleteUser(filter: $filter, owner: $owner) {
+      id
+      firstName
+      lastName
+      email
+      countryCode
+      mobileNumber
+      password
+      userType
+      loginType
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
